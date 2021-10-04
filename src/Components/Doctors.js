bb23fc670095340f7d76f51cea0fb494111dad98
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-10-04 20:04:10 +0700

    Update database and styling for Doctors Component
    Memenuhi:
    Modul 1: Ternary, onClick, Scope, Function
    Modul3: doctors, modal, detail state, render lifecycle, OpenModal onClick event handling.
    Modul4: useState, useContext, useEffect
    Modul5: fake api json-server, modal onclick dokter

--- src/Components/Doctors.js
+++ src/Components/Doctors.js
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import Typography from "@mui/material/Typography";
+import { CardContent, CardMedia } from "@mui/material";
 
 const DoctorsDayContext = createContext();
 Modal.setAppElement("#root");
@@ -52,11 +53,14 @@ function Data() {
   const [currentDetail, setCurrentDetail] = useState({});
   const day = useContext(DoctorsDayContext);
 
-  function OpenModal(nama) {
+  function OpenModal(id, nama, spesialis, ruang, jam) {
     setCurrentDetail({
-      nama: nama,
+      ID: id,
+      Nama: nama,
+      Spesialis: spesialis,
+      Ruang: ruang,
+      Jam: jam,
     });
-
     setIsOpen(true);
   }
 
@@ -83,23 +87,58 @@ function Data() {
 
   return (
     <div>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         {doctors.map((result) => {
           return (
-            <Grid item key={result.id} md={4}>
+            <Grid item key={result.id} xs={12}>
               <Card variant="outlined">
                 <CardActionArea
-                  onClick={() => OpenModal(result.id, result.nama)}
+                  onClick={() =>
+                    OpenModal(
+                      result.id,
+                      result.nama,
+                      result.spesialis,
+                      result.ruang,
+                      result.jam
+                    )
+                  }
                 >
-                  <Typography variant="h5">{result.id}</Typography>
-                  <Typography variant="h5">{result.nama}</Typography>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={
+                      Math.random() * 100 >= 50
+                        ? "https://i.ibb.co/Dwctwbh/doktor2-removebg-preview.png"
+                        : "https://i.ibb.co/51dVPch/doctor1.png"
+                    }
+                    alt="doctor"
+                  />
+                  <CardContent>
+                    <Typography variant="h5">Nama: {result.nama}</Typography>
+                    <Typography variant="h5">
+                      Ruang Praktek:{result.ruang}
+                    </Typography>
+                    <Typography variant="h5">
+                      Jam Praktek:{result.jam}
+                    </Typography>
+                  </CardContent>
                 </CardActionArea>
               </Card>
             </Grid>
           );
         })}
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-          <Typography>{currentDetail.nama}</Typography>
+          <Typography variant="h3">
+            Detail Praktik Doktor {currentDetail.Nama}
+          </Typography>
+          <br></br>
+          <Typography variant="h4">ID: {currentDetail.ID}</Typography>
+          <Typography variant="h4">Nama: {currentDetail.Nama}</Typography>
+          <Typography variant="h4">
+            Spesialis: {currentDetail.Spesialis}
+          </Typography>
+          <Typography variant="h4">Ruang: {currentDetail.Ruang}</Typography>
+          <Typography variant="h4">Jam Praktek: {currentDetail.Jam}</Typography>
         </Modal>
       </Grid>
     </div>
